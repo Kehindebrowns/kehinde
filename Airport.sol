1ea1c6e1c209633cd2e72c7b9c49aeabf01c7263
Kehindebrowns <111283128+Kehindebrowns@users.noreply.github.com>
2023-08-19 16:05:30 +0100

    Add files via upload

---
+++ Airport.sol
@@ -0,0 +1,56 @@
+// SPDX-Lisence-Identifier:MIT
+  pragma solidity ^0.8.0;
+
+   contract MyAirport{
+    mapping (uint => string)public names ; // random passangers
+       //names [1] = 'Mina';
+       names[2] = 'tola';
+       names [3] =  'fola';
+       names [4] =  'kemi';
+       names [5] =  'smith';
+       names [6] = 'nataniel';
+       names [7] =  'zion'; 
+       names [8] = 'faithful';
+       names [9] = 'funmi';
+       names [10] = 'honour';
+       names [11] = 'john';
+       names [12] =  'peter';
+       unit[] public myUnit =[1,2,3,4,5,6,7,8,9,10,11,12];
+             function addValue  (String memeory _value) public view return (uint){
+                return.push (_Value);
+                _;
+   }
+       function getCount () public view return{
+        return ++;
+
+        function Incrementcount() public return{
+            return = +1;
+        }
+         
+         Enums Status{
+            passangers;
+            planes;
+         }
+         structs Items_Schema{
+            Uint256 _1d;
+            string _passangers;
+            unit  _plane;
+            address payable public My address = 000000000000000000000000000000000000000;
+            
+             }   
+              function ValueCount() publi view return (unit){ 
+                return Value.Lenght;
+                _;
+              }
+                
+         }
+         
+
+            
+                }
+
+           
+        
+       
+      
+ 
